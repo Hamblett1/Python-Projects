1a55db1b7734d86113c9687436320f19873f693c
HARRY HAMBLETT <HARRY.HAMBLETT1@studytafensw.edu.au>
2023-02-08 16:52:59 +1100

    Initial commit

---
+++ Week 01/intro.py
@@ -0,0 +1,12 @@
+print("Welcome Everyone")
+# This is a variable
+name = "Harry"
+# String is a data type that should always be surrounded by double quotes "string"
+# we can combine a variable and a string in the print () function by separating them using a coma
+print("I hope your having a nice day", name, ".")
+
+# Another data type is a number (no quotes needed)
+Age = 70
+print(name, "is", Age)
+
+
